d4ecfb492533d3216f2205decedc9825b35a6763
DuStein <steindu@gmail.com>
2022-06-11 23:11:36 -0300

    funcoes desenvolvidas

--- script.js
+++ script.js
@@ -2,35 +2,28 @@
 function pcPlay() {
   let gamePlays = ["pedra", "papel", "tesoura"];
   let pcChoice = Math.floor(Math.random()*gamePlays.length);
-  console.log("computador: " + gamePlays[pcChoice])
+  // console.log("computador: " + gamePlays[pcChoice])
   return gamePlays[pcChoice];
 };
 
 function playRound(computer, userInput) {
 
   let user = userInput.toLowerCase().trim();
-
+  let jogadas = `Você escolheu ${user}, o computador escolheu ${computer}... `
   if (computer === "pedra" && user === "papel") {
-    console.log("user GANHOU! Papel enrola Pedra!");
-    return 2
+    return jogadas + "user GANHOU! Papel enrola Pedra!"
   } else if (computer === "pedra" && user === "tesoura") {
-    console.log("computer VENCEU! Pedra quebra a Tesoura")
-    return 1
+    return jogadas + "computer VENCEU! Pedra quebra a Tesoura"
   } else if (computer === "papel" && user === "pedra") {
-    console.log("computer VENCEU! Papel enrola a Pedra")
-    return 1
+    return jogadas + "computer VENCEU! Papel enrola a Pedra"
   } else if (computer === "papel" && user === "tesoura") {
-    console.log("user GANHOU! Tesoura corta Papel")
-    return 2
+    return jogadas + "user GANHOU! Tesoura corta Papel"
   } else if (computer === "tesoura" && user === "papel") {
-    console.log("computer VENCEU! Tesoura corta Papel")
-    return 1
+    return jogadas + "computer VENCEU! Tesoura corta Papel"
   } else if (computer === "tesoura" && user === "pedra") {
-    console.log("user GANHOU! Pedra quebra a Tesoura")
-    return 2
+    return jogadas + "user GANHOU! Pedra quebra a Tesoura"
   } else {
-    console.log("Deu EMPATE ! Escolheram a mesma aposta...")
-    return 3
+    return jogadas + "Deu EMPATE ! Escolheram a mesma aposta..."
   }
 }
 
